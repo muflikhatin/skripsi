ff408eea786bc4646106ba367115ccf6f36f3e8a
muflikhatin <115399427+muflikhatin@users.noreply.github.com>
2025-06-24 00:16:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from main import main as main_page
 # Sidebar untuk navigasi
 selected_page = st.sidebar.selectbox(
     "Pilih Halaman:",
-    ["Halaman Utama", "Halaman Crawling Data",
-     "Halaman Preprocessing Data", "Halaman Term Frequency","Halaman K Means","Halaman lda knn kmeans", "Halaman Topic Modelling", "Halaman closeness centrality crawling", "Halaman closeness centrality", "Halaman Klasifikasi_SVM"])
+    ["Halaman Utama"])
 
 # Menampilkan konten halaman yang dipilih
 if selected_page == "Halaman Utama":
